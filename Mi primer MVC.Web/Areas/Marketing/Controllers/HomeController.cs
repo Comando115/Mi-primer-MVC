9e3046f03f08ed3c71aa9146009572b8fac21756
Comando115 <90572759+Comando115@users.noreply.github.com>
2021-11-26 01:14:09 -0500

    GetJsonProducts

--- Mi primer MVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ Mi primer MVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -1,17 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mi_primer_MVC.Web.Areas.Marketing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mi_primer_MVC.Web.Areas.Marketing.Controllers
 {
+    [Area("Marketing")]
     public class HomeController : Controller
     {   
-        [Area("Marketing")]
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+
+        public IEnumerable<Product> GetJsonProducts() {
+
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\products.json");
+
+            var Json = System.IO.File.ReadAllText(folder);
+
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(Json);
+
+            return products;
+        
         }
     }
 }
---
+++ Mi primer MVC.Web/Areas/Marketing/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_primer_MVC.Web.Areas.Marketing.Models
+{
+    public class Product
+    {
+
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        public string Img { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
